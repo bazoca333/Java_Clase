e6315c1bbbe5161372664c48b965834a2299d4ef
Álvaro Millanes <149728673+bazoca333@users.noreply.github.com>
2024-02-10 21:56:47 +0100

    Update Devolucio_compra.java

--- Eclipse/Java_compras/src/Devolucio_compra.java
+++ Eclipse/Java_compras/src/Devolucio_compra.java
@@ -7,9 +7,9 @@ public class Devolucio_compra {
 	private static Scanner scan_s = new Scanner(System.in);
 	
 	private static String numTiquet = "";
+	private static int numlin = 0;
 
 public static void main(String[] args) {
-	boolean tiquetEncontrado = false;
 	boolean continuar = true;
 	
 	while (continuar == true) {
@@ -22,25 +22,51 @@ public static void main(String[] args) {
         numTiquet = scan_s.nextLine().toUpperCase();
         
 		if (numTiquet.equals("XXX")) {
-	       	 tiquetEncontrado = false;
 	       	 continuar = false;
+	       	menu.main(args);
 			}else {
 				String x[] = {"NUMT"};
 		    	String[] checkTiq = bbdd.select(con, "SELECT numt FROM PRF_TIQUET WHERE numt = " + numTiquet, x);
 		        if (checkTiq.length > 0) {
-		        	String y[] = {"NOMPR", "PREUD1", "STOCK"};
-		        	bbdd.print(con, "SELECT NOMPR, PREUD1, STOCK FROM PRF_PRODUCT WHERE codbarres = '" + numTiquet + "'", y);
-				    
-		        	tiquetEncontrado = true;
 		        	
-				}else {
+		        	String y[] = {"NUMT", "DATAT", "IDE", "CLIENTE", "IMPORT_TOT", "PUNTS_TIQ"};
+		        	bbdd.print(con, "SELECT NUMT, DATAT, IDE, CLIENTE, IMPORT_TOT, PUNTS_TIQ FROM PRF_TIQUET WHERE numt = '" + numTiquet + "'", y);
+				    
+		        	System.out.println();
+		        	nova_compra.mostrarLineas(Integer.parseInt(numTiquet), numlin);
+		        	continuar = false;
+		        	
+		        	System.out.println("Está seguro de eliminar el tiquet?");
+		        	System.out.println("1. Sí");
+		        	System.out.println("2. No");
+		        	
+		        	continuar = true;
+		        	while (continuar) {
+		        		int respuesta = nova_compra.getInput(scan);
+		        		if (respuesta == 1) {
+		        			System.out.println("Se eliminarán todas las lineas y el tiquet");
+		        			bbdd.delete(con, "DELETE FROM PRF_LINTIQ WHERE numtiq = " + numTiquet);
+		        			bbdd.delete(con, "DELETE FROM PRF_TIQUET WHERE numt = " + numTiquet);
+		        			continuar = false;
+		        		}else if (respuesta == 2) {
+		        			System.out.println("Se cancela el delete");
+		        			Devolucio_compra.main(args);
+		        			
+		        		}else {
+		        			System.out.println("Por favor, ponga una de las 2 opciones");
+		        		}
+		        	}
+		        	menu.main(args);
+				
+		        }else {
 	                System.out.println("El producto no existe");
 				}
         
 
     	
 	}
-	System.out.println("hola");
+
+	
 
 }//Fin while
 }
